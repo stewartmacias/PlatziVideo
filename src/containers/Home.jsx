0adfd89af6f9e1104d6feee1d7f282c3eba586c9
Stewart Macias <macias.stewart@gmail.com>
2022-02-11 10:53:41 -0500

    delete from myList

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -14,7 +14,11 @@ const Home = ({ myList, trends, originals }) => {
         <Categories title='Mi Lista'>
           <Carousel>
             {myList.map(item => (
-              <CarouselItem key={item.id} {...item} />
+              <CarouselItem
+                key={item.id}
+                {...item}
+                isList
+              />
             ))}
           </Carousel>
         </Categories>
@@ -22,7 +26,10 @@ const Home = ({ myList, trends, originals }) => {
       <Categories title='Tendencias'>
         <Carousel>
           {trends.map(item => (
-            <CarouselItem key={item.id} {...item} />
+            <CarouselItem
+              key={item.id}
+              {...item}
+            />
           ))}
         </Carousel>
       </Categories>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,20 @@
+/* eslint-disable no-case-declarations */
 const reducer = (state, action) => {
   switch (action.type) {
     case 'SET_FAVORITE':
+      const found = state.myList.find(index => index.id === action.payload.id);
+      if (found) {
+        return { ...state };
+      }
       return {
         ...state,
         myList: [...state.myList, action.payload],
+      };
+
+    case 'DELETE_FAVORITE':
+      return {
+        ...state,
+        myList: state.myList.filter(items => items.id !== action.payload),
       };
     default:
       return state;
