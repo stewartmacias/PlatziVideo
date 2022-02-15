79e1204b007bb1b61499f4e41505161f0f9186af
Stewart Macias <macias.stewart@gmail.com>
2022-02-15 14:40:03 -0500

    redux debug tools

--- src/index.js
+++ src/index.js
@@ -171,7 +171,8 @@ const initialState = {
   ],
 };
 
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducer, initialState, composeEnhancers);
 
 ReactDOM.render(
   <Provider store={store}>
